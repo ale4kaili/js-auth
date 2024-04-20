e40c6f416d38d2e4ef3afa4ce933c92904cfc9aa
Alyona Lapinskaya <alena.ilinskaya1201@gmail.com>
2024-04-20 06:47:38 +0300

    lesson-5-auth

---
+++ src/script/list.js
@@ -0,0 +1,23 @@
+class List {
+    STATE = {
+        LOADING: 'loading',
+        SUCCESS: 'success',
+        ERROR: 'error',
+    }
+
+    status = null
+    data = null
+    element = null
+
+    updateStatus = (status, data) => {
+        this.status = status
+
+        if(data) this.data = data
+
+        this.updateView()
+    }
+}
+
+module.exports = {
+    List,
+  }
